05b8544d42750f2936c5d6f5b137a12e249c01d8
Akinola David Olawale <akinoladavidolawale@gmail.com>
2024-05-20 22:37:46 +0100

    These classes files inherited from BaseModel(base_model.py)

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is the Amenity class module"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """The Amenity class that inherits from BaseModel"""
+    name = ''
